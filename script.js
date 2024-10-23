ad2d8910b489b4e32489d7e9e5133cc987088aad
Dylan T <106625299+beepboopdylan@users.noreply.github.com>
2024-10-23 14:55:00 -0400

    trying to add typewriter effect

---
+++ script.js
@@ -0,0 +1,30 @@
+// script.js
+
+var i = 0;
+var txt = 
+`<span class="keyword">def</span> <span class="function">dylan_tran</span><span class="symbols">():</span><br>
+<p><span class="text"># I am a junior in </span></p>
+<p><span class="interests">interests = [</span>
+<span class="text">linguistics</span><span class="keyword">,</span> <span class="text">hello</span><span class="keyword">,</span> 
+<span class="text">cryptography</span><span class="keyword">,</span> <span class="text">and other stuff</span>
+<span class="interests">]</span></p>
+<p><span class="interests">languages = [</span>
+<span class="text">python</span><span class="keyword">,</span> <span class="text">java</span><span class="keyword">,</span> 
+<span class="text">c</span><span class="keyword">,</span> <span class="text">sql</span><span class="keyword">,</span> 
+<span class="text">html</span><span class="keyword">,</span> <span class="text">css</span><span class="keyword">,</span> 
+<span class="text">Arabic</span><span class="interests">]</span></p>`;
+var speed = 50;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("typewriter-content").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+// Call the typewriter function when the page loads
+window.onload = function() {
+  typeWriter();
+};
+
