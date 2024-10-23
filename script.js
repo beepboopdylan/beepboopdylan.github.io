cadc3543e7254074aadfb849b55e99f2e76cb926
Dylan T <106625299+beepboopdylan@users.noreply.github.com>
2024-10-23 15:21:53 -0400

    Trying to implement typewriter

--- script.js
+++ script.js
@@ -12,14 +12,16 @@ var txt =
 <span class="text">c</span><span class="keyword">,</span> <span class="text">sql</span><span class="keyword">,</span> 
 <span class="text">html</span><span class="keyword">,</span> <span class="text">css</span><span class="keyword">,</span> 
 <span class="text">Arabic</span><span class="interests">]</span></p>`;
-var speed = 50;
+var speed = 20;
+var step = 3;
 
 function typeWriter() {
-  if (i <= txt.length) {
-    // Update innerHTML by slicing the txt variable to get progressively more of the string
-    document.getElementById("typewriter-content").innerHTML = txt.slice(0, i);
-    i++;
+  if (i < txt.length) {
+    document.getElementById("typewriter-content").innerText += txt.substring(i, i + step);
+    i += step;
     setTimeout(typeWriter, speed);
+  } else {
+    document.getElementById("typewriter-content").innerHTML = txt;  // Switch to innerHTML to render the HTML properly after typing is complete
   }
 }
 
