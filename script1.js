6d00cc8e144b8c7a8fbd73dd55d2d06f3380733d
Dylan T <106625299+beepboopdylan@users.noreply.github.com>
2024-10-26 10:27:00 -0400

    trying to add navbar to page

--- script1.js
+++ script1.js
@@ -1,13 +1,18 @@
-let lastScrollTop = 0;
-const resumeButton = document.querySelector('.resume');
+let lastScroll = 0;
+
+const navbar = document.querySelector('.navbar');
 
 window.addEventListener('scroll', function() {
-    const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
-    if (currentScroll > lastScrollTop) {
-        resumeButton.style.opacity = '0';
-    } else {
-        // Scrolling up
-        resumeButton.style.opacity = '1'; // Show the button
-    }
-    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For Mobile or negative scrolling
+  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  if (scrollTop > lastScroll) {
+    navbar.classList.add('hidden');
+  } else {
+    navbar.classList.remove('hidden');
+  }
+  lastScroll = scrollTop;
+
 });
+
+
+
+
