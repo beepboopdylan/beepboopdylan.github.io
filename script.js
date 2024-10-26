315a590cfb984f4531b58866152a42bafe96db7a
Dylan T <106625299+beepboopdylan@users.noreply.github.com>
2024-10-26 03:47:07 -0400

    added new projects functionality

--- script.js
+++ script.js
@@ -1,31 +1,29 @@
+let lastScrollTop = 0;
+const homeButton = document.querySelector('.home-button'); // Select the home button
 
-var i = 0;
-var txt = 
-`<span class="keyword">def</span> <span class="function">dylan_tran</span><span class="symbols">():</span><br>
-<p><span class="text"># I am a junior in </span></p>
-<p><span class="interests">interests = [</span>
-<span class="text">linguistics</span><span class="keyword">,</span> <span class="text">hello</span><span class="keyword">,</span> 
-<span class="text">cryptography</span><span class="keyword">,</span> <span class="text">and other stuff</span>
-<span class="interests">]</span></p>
-<p><span class="interests">languages = [</span>
-<span class="text">python</span><span class="keyword">,</span> <span class="text">java</span><span class="keyword">,</span> 
-<span class="text">c</span><span class="keyword">,</span> <span class="text">sql</span><span class="keyword">,</span> 
-<span class="text">html</span><span class="keyword">,</span> <span class="text">css</span><span class="keyword">,</span> 
-<span class="text">Arabic</span><span class="interests">]</span></p>`;
-var speed = 20;
-var step = 3;
+window.addEventListener('scroll', function() {
+    const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+    if (currentScroll > lastScrollTop) {
+        homeButton.style.opacity = '0';
+    } else {
+        // Scrolling up
+        homeButton.style.opacity = '1'; // Show the button
+    }
+    // lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For Mobile or negative scrolling
+});
 
-function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("typewriter-content").innerText += txt.substring(i, i + step);
-    i += step;
-    setTimeout(typeWriter, speed);
-  } else {
-    document.getElementById("typewriter-content").innerHTML = txt;  // Switch to innerHTML to render the HTML properly after typing is complete
-  }
-}
+//scroll back up to top!!
+const scroll_up = document.getElementById("topButton");
 
-// Call the typewriter function when the page loads
-window.onload = function() {
-  typeWriter();
+window.onscroll = function() {
+   if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+      scroll_up.classList.add("show");
+   } else {
+      scroll_up.classList.remove("show");
+   }
 };
+
+scroll_up.addEventListener('click', function() {
+  window.scrollTo(0, 0);
+});
+
